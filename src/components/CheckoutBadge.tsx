e2287cd391103d4a3aeec753fbacf90e6b4fcd7f
Antoine Sion <antoine@archimaide.fr>
2024-06-18 15:12:47 +0200

    css module styling

--- src/components/CheckoutBadge.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const CheckoutBadge: React.FC = () => {
-  return <p>Hello world!</p>;
-};
---
+++ src/components/CheckoutBadge/CheckoutBadge.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./CheckoutBadge.module.css";
+
+export const CheckoutBadge: React.FC = () => {
+  return <p className={styles.badge}>Hello world!</p>;
+};
