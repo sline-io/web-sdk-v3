80b8d02438251fd62f0cd14019fd740890f07399
Antoine Sion <antoine@archimaide.fr>
2024-06-18 15:16:59 +0200

    root imports

--- src/components/CheckoutBadge/CheckoutBadge.tsx
+++ src/components/CheckoutBadge/CheckoutBadge.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CheckoutBadge.module.css";
+import { sum } from "utils";
 
 export const CheckoutBadge: React.FC = () => {
-  return <p className={styles.badge}>Hello world!</p>;
+  return <p className={styles.badge}>Sum: {sum(10, 5)}</p>;
 };
