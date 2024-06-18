d2b3be67f291e17ba79e0610c8af11d290e7eff9
Antoine Sion <antoine@archimaide.fr>
2024-06-18 15:36:29 +0200

    update container

--- src/components/CheckoutBadge/CheckoutBadge.tsx
+++ src/components/CheckoutBadge/CheckoutBadge.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./CheckoutBadge.module.css";
 import { sum } from "utils";
 
-export const CheckoutBadge: React.FC = () => {
-  return <p className={styles.badge}>Sum: {sum(10, 5)}</p>;
+interface Props {
+  x: number;
+  y: number;
+}
+
+export const CheckoutBadge: React.FC<Props> = ({ x, y }) => {
+  return <p className={styles.badge}>Sum: {sum(x, y)}</p>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,30 @@ import ReactDOM from "react-dom/client";
 import { CheckoutBadge } from "./components";
 import React from "react";
 
-export const addCheckoutBadge = (containerId: string) => {
+const rootByContainerId: Record<string, ReactDOM.Root> = {};
+
+const getOrCreateRoot = (containerId: string) => {
+  if (containerId in rootByContainerId) {
+    return rootByContainerId[containerId];
+  }
+
   const container = document.getElementById(containerId);
 
   if (!container) throw Error(`Container #{containerId} not found in DOM.`);
 
   const root = ReactDOM.createRoot(container);
 
+  rootByContainerId[containerId] = root;
+
+  return root;
+};
+
+export const addCheckoutBadge = (containerId: string, x: number, y: number) => {
+  const root = getOrCreateRoot(containerId);
+
   root.render(
     <React.StrictMode>
-      <CheckoutBadge />
+      <CheckoutBadge x={x} y={y} />
     </React.StrictMode>
   );
 };
